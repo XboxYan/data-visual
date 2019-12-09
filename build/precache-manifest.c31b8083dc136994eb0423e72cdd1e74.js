51e011f4b6bc508e8fcc928c6b9c1bca0e696fc8
XboxYan <yanwenbin1991@live.com>
2019-12-09 19:06:10 +0800

    build

---
+++ build/precache-manifest.c31b8083dc136994eb0423e72cdd1e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756e991aab52097c99f5a732a1b3eb91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "370626871d150e959c03",
+    "url": "/static/css/2.043e2351.chunk.css"
+  },
+  {
+    "revision": "39833a5ebf4247c215b6",
+    "url": "/static/css/main.382fb602.chunk.css"
+  },
+  {
+    "revision": "370626871d150e959c03",
+    "url": "/static/js/2.41e45f4f.chunk.js"
+  },
+  {
+    "revision": "39833a5ebf4247c215b6",
+    "url": "/static/js/main.cdf9fffb.chunk.js"
+  },
+  {
+    "revision": "98dd5a686b7f89257b2a",
+    "url": "/static/js/runtime-main.51fb1e29.js"
+  }
+]);
